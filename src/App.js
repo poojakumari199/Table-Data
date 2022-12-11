d90463a103097c7b72e55b361a198e9046a900d3
Pooja Shrivastava <poo1234.kumari@gmail.com>
2022-12-11 11:25:05 +0530

    displaying table data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+
+  const tableData = async () => {
+    const api_url = 'https://jsonplaceholder.typicode.com/users';
+    const response = await fetch(api_url)
+    const data = await response.json();
+    console.log("pk", data);
+ 
+  if(data.length > 0){
+    let temp = [];
+     data.forEach((item) => {
+       temp += "</tr>";
+       temp += "<td>" + item.id + "</td>" ;
+       temp += "<td>" + item.name + "</td>";
+       temp += "<td>" + item.username + "</td>";
+       temp += "<td>" + item.email + "</td>";
+       temp += "<td>" + item.address.street + "</td>";
+       temp += "<td>" + item.company.name + "</td>";
+       });
+     document.getElementById("data").innerHTML += temp;
+
+  }
+ 
+  }
+useEffect (() => {
+  tableData();
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">         
+     <h1>TABLE</h1>
+     <div id="container">
+     <table>
+       <thead>
+       <tr>
+       <th>ID</th>
+       <th>NAME</th>
+       <th>USERNAME</th>
+       <th>EMAIL</th>
+       <th>ADDRESS</th>
+       <th>COMPANY</th>
+       </tr>
+       </thead>
+       <tbody id="data"></tbody>
+           </table>
+           </div>
     </div>
   );
 }
